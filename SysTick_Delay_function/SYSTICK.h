5776271dcf2107403d3178cb39b3b96278f7a4bb
shawky2002020 <131324698+shawky2002020@users.noreply.github.com>
2023-05-04 14:59:58 +0300

    Create SYSTICK.h

---
+++ SysTick_Delay_function/SYSTICK.h
@@ -0,0 +1,9 @@
+// SysTick intialization
+void init_Timer_delay(void);
+
+//delay Functions using SysTick
+void Delay_Of_Fractions(int frac);
+void delay(int s, int d);
+
+//delay MicroSeconds
+void hundredMicroSecounds(void);
